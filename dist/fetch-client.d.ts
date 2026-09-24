e9eab92970a0cbeee7666f1593d76c5496b6f399
lamartinecabral <lamartine.cb@gmail.com>
2026-09-24 20:42:45 -0300

    remove dist

--- dist/fetch-client.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SearchClient } from "./utils.js";
-export declare function isDuckduckgoAvailable(): Promise<boolean>;
-declare const client: SearchClient;
-export default client;
--- dist/tavily-client.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SearchClient } from "./utils.js";
-export declare const setTavilyApiKey: (value: string) => void;
-declare const client: SearchClient;
-export default client;
